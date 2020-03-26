fd5d3647e3233393366873b47b9570548d8d1785
Imre NAGY <crash5@users.noreply.github.com>
2020-03-26 23:33:28 +0100

    Properly handle big html source from facebook page

--- FacebookVideoDownloader.py
+++ FacebookVideoDownloader.py
@@ -1,4 +1,6 @@
 from module.plugins.internal.Hoster import Hoster
+from module.network.HTTPRequest import HTTPRequest
+from module.network.CookieJar import CookieJar
 
 import re
 
@@ -11,6 +13,17 @@ class FacebookVideoDownloader(Hoster):
     __config__ = [
         ('activated', 'bool', 'Activated', True)
     ]
+
+    def setup(self):
+        try:
+            self.req.http.close()
+        except Exception:
+            pass
+
+        self.req.http = BIGHTTPRequest(
+            cookies=CookieJar(None),
+            options=self.pyload.requestFactory.getOptions(),
+            limit=5000000)
 
     def init(self):
         self.sd_url_pattern = r'sd_src:"(.+?)"'
@@ -29,4 +42,30 @@ class FacebookVideoDownloader(Hoster):
             pyfile.name = '%s.%s' % (title_match.group(1).decode('utf8'), 'mp4')
 
         self.download(sd_url)
+
+
+# From youtube hoster because the html source is too big for the load method
+class BIGHTTPRequest(HTTPRequest):
+    """
+    Overcome HTTPRequest's load() size limit to allow
+    loading very big web pages by overrding HTTPRequest's write() function
+    """
+
+    # @TODO: Add 'limit' parameter to HTTPRequest in v0.4.10
+    def __init__(self, cookies=None, options=None, limit=2000000):
+        self.limit = limit
+        HTTPRequest.__init__(self, cookies=cookies, options=options)
+
+    def write(self, buf):
+        """ writes response """
+        if self.limit and self.rep.tell() > self.limit or self.abort:
+            rep = self.getResponse()
+            if self.abort:
+                raise Abort()
+            f = open("response.dump", "wb")
+            f.write(rep)
+            f.close()
+            raise Exception("Loaded Url exceeded limit")
+
+        self.rep.write(buf)
 
